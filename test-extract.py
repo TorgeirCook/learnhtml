58c604813dd229f36e0d1095d03120c69f77a1d8
Torgeir Pedersen Cook <torgeirpc@gmail.com>
2021-03-07 13:27:24 +0100

    Prints extracted html to file

--- test-extract.py
+++ test-extract.py
@@ -1,15 +1,27 @@
 import pickle
 from learnhtml.extractor import HTMLExtractor
-import requests
 import webpage2html
+from lxml import etree
 
-with open('model2.pkl', 'rb') as f:
+with open('models/model-recipe.pkl', 'rb') as f:
     model = pickle.load(f)
 
 extractor = HTMLExtractor(model)
 
-url = 'https://herbsutter.com/'
+url = 'https://trinesmatblogg.no/recipe/kremet-kyllingform-med-paprika'
 html = webpage2html.generate(url, verbose=False)  # webpage2html downloads all dependencies of the page as well
-data = extractor.extract_from_html(html)
+paths = extractor.extract_from_html(html)
 
-print(data)
+print("extracted paths", paths)
+
+root = etree.HTML(html.encode('utf-8'))
+
+extracted_html = []
+for path in paths:
+    elements = root.xpath(path)
+    for element in elements:
+        extracted_html.append(etree.tostring(element, encoding='unicode', pretty_print=True))
+
+with open('test.html', 'w') as f:
+    for item in extracted_html:
+        f.write("%s\n" % item)
